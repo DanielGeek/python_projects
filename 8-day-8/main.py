a1f8c2f52c560fc2891ac935162169637c2a60a4
Daniel <daniel30081990@gmail.com>
2023-01-28 23:20:58 -0300

    82. Positional vs. Keyword Arguments

--- 8-day-8/main.py
+++ 8-day-8/main.py
@@ -12,3 +12,13 @@ def greet_with_name(name):
     print(f"How do you do {name}")
 
 greet_with_name("Daniel")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Jack Bauer", "Nowhere")
+
+#Functions with keywod arguments
+greet_with(location="London", name="Angel")
