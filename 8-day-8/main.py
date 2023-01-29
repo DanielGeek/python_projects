39e7f1791ec45474ba6ad6550de51921c330517f
Daniel <daniel30081990@gmail.com>
2023-01-28 23:07:39 -0300

    81. Functions with Inputs

---
+++ 8-day-8/main.py
@@ -0,0 +1,14 @@
+#Simple Function
+def greet():
+    print("Hello Angela")
+    print("How do you do Jack Bauer?")
+    print("Isn't the weather nice today?")
+greet()
+
+#Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Daniel")
